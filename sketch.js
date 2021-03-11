3733d6daa9edb43255d5ac232d88b2dcdda9e96e
Kush <diyakush129@gmail.com>
2021-03-11 15:50:11 -0500

    Learner

--- sketch.js
+++ sketch.js
@@ -9,8 +9,18 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var car1_Img, car2_Img,car3_Img,car4_Img;
+var track, ground
 
+function preload(){
+car1_Img = loadImage("images/car1.png");
+car2_Img = loadImage("images/car2.png");
+car3_Img = loadImage("images/car3.png");
+car4_Img = loadImage("images/car4.png");
+track = loadImage("images/track.jpg");
+ground = loadImage("images/ground.png");
 
+}
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
@@ -27,5 +37,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
