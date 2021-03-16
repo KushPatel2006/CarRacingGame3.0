192aa3991a38a156aa26532b97a32decb1798d05
Kush <diyakush129@gmail.com>
2021-03-16 15:28:24 -0400

    Learner

--- js/Form.js
+++ js/Form.js
@@ -5,6 +5,7 @@ class Form {
     this.button = createButton('Play');
     this.greeting = createElement('h2');
     this.title = createElement('h2');
+    this.reset = createButton('Reset');
   }
   hide(){
     this.greeting.hide();
@@ -19,8 +20,8 @@ class Form {
 
     this.input.position(displayWidth/2 - 40 , displayHeight/2 - 80);
     this.button.position(displayWidth/2 + 30, displayHeight/2);
-2
-    this.button.mousePressed(()=>{
+    this.reset.position(displayWidth-100, 20);
+    this.button.mousePressed((y)=>{
       this.input.hide();
       this.button.hide();
       player.name = this.input.value();
@@ -31,6 +32,9 @@ class Form {
       this.greeting.html("Hello " + player.name)
       this.greeting.position(displayWidth/2 - 70, displayHeight/4);
     });
-
+      this.reset.mousePressed(()=>{
+        game.update(0);
+        player.updateCount(0);
+      })
   }
 }
